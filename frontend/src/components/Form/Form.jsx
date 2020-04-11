a0e1e393df0b8d96172a294ae3d4accc215a7b08
Kevin He <yc4he@uwaterloo.ca>
2020-04-11 16:09:04 +0800

    added title scraping and loading sign

--- frontend/src/components/Form/Form.jsx
+++ frontend/src/components/Form/Form.jsx
@@ -1,9 +1,14 @@
 import React from 'react'; 
+import 'font-awesome/css/font-awesome.min.css';
 
 class Form extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {url: ''};
+    state = {
+        loading:'none',
+        url: '',
+        display:'none',
+        message:'',
+        title:'',
+        prediction:''
     }
 
     handleChange = event => {
@@ -11,32 +16,43 @@ class Form extends React.Component{
     }
 
     handleSubmit = event => {
+        this.setState({loading:'block'})
         fetch('/parse_url', {
             method: 'POST',
-            body: JSON.stringify(this.state),
+            body: JSON.stringify({"url":this.state.url}),
         }).then(res => res.json())
             .then(data => {
-                document.getElementById("article_render").style.display = "block";
-                document.getElementById("article_text").value = data.message;
-                document.getElementById("result").innerText = data.prediction;
+                this.setState({
+                    loading:'none',
+                    display:'block',
+                    message: data.message,
+                    title:data.title,
+                    prediction:data.prediction
+                });
             })
             .catch(err => console.log("Error:", err));
         event.preventDefault();
     }
 
     render(){
+        const {loading} = this.state.loading;
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
                     <label>Input the news article link here: <br/></label>
                     <input id="input-url" placeholder="Paste your article link here" value={this.state.url} onChange={this.handleChange} />
-                    <input type="submit" value="Submit" />            
+                    <input type="submit" value="Submit" />  
+                    {/* <button className="button" disabled={!loading}> */}
+                    <div style={{display:this.state.loading}}><i className="fa fa-refresh fa-spin" />   Loading...</div>
+                    {/* </button>           */}
                 </form>
-                <div id="article_render" style={{display: "none"}}>
+                <div id="article_render" style={{display: this.state.display}}>
+                    <p>The title of your article:</p>
+                    <div id="article_title" >{this.state.title}</div>
                     <p>Your article</p>
-                    <textarea id="article_text" rows="30" cols="70" readOnly/>
+                    <textarea id="article_text" rows="30" cols="70" readOnly value = {this.state.message} />
                     <div>The result of the ML detection:</div>
-                    <div id="result"></div>
+                    <div id="result">{this.state.prediction}</div>
                 </div>
             </div>
         )
